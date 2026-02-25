9789b5a39d802f1ea39825233ee06c7316fe7796
OlgaVino25 <olgavinokyrova25@yandex.ru>
2026-02-25 17:37:25 +0300

    json

---
+++ load_questions_to_redis.py
@@ -0,0 +1,18 @@
+import json
+import redis
+from settings import REDIS_HOST, REDIS_PORT, REDIS_DB
+
+
+r = redis.Redis(host=REDIS_HOST, port=REDIS_PORT, db=REDIS_DB, decode_responses=True)
+
+with open("questions.json", "r", encoding="utf-8") as f:
+    quiz_dict = json.load(f)
+
+r.delete("question_ids")
+
+for idx, (q, a) in enumerate(quiz_dict.items()):
+    key = f"question:{idx}"
+    r.hset(key, mapping={"question": q, "answer": a})
+    r.sadd("question_ids", idx)
+
+print(f"Загружено {len(quiz_dict)} вопросов. Последний ID: {idx}")
